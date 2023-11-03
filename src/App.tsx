24a505e355e8381c98cdf5c65f2be05c93dfcb8d
Deepak mandal <nomercyhunters@gmail.com>
2023-11-03 14:10:12 +0530

    portfolio optimised by code sppliting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
-import Navbar from './components/NAVBAR/Navbar';
+// import Navbar from './components/NAVBAR/Navbar';
 import { Box } from "@chakra-ui/react"
-import Home from './PAGE/HOME/Home';
-import Education from './PAGE/EDUCATION/Education';
-import Projects from './PAGE/PROJECTS/Projects';
-import Contact from './PAGE/CONTACT/Contact';
+// import Home from './PAGE/HOME/Home';
+// import Education from './PAGE/EDUCATION/Education';
+// import Projects from './PAGE/PROJECTS/Projects';
+// import Contact from './PAGE/CONTACT/Contact';
+import { Suspense } from 'react';
 
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Splash from './PAGE/SPLASH/Splash';
@@ -13,13 +14,15 @@ import { appTheme } from './components/theme/theme';
 import Footer from './components/Footer/Footer';
 import TopButton from './components/TopButton/TopButton';
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Experience from './PAGE/EXPERIENCE/Experience';
+// import Experience from './PAGE/EXPERIENCE/Experience';
 import Reveal from 'react-awesome-reveal';
 import { keyframes } from "@emotion/react";
-// const Home = React.lazy(() => import("./PAGE/HOME/Home"));
-// const Education = React.lazy(() => import("./PAGE/EDUCATION/Education"))
-// const Projects = React.lazy(() => import("./PAGE/PROJECTS/Projects"))
-// const Contact = React.lazy(() => import("./PAGE/CONTACT/Contact"))
+const Home = React.lazy(() => import("./PAGE/HOME/Home"));
+const Education = React.lazy(() => import("./PAGE/EDUCATION/Education"))
+const Projects = React.lazy(() => import("./PAGE/PROJECTS/Projects"))
+const Contact = React.lazy(() => import("./PAGE/CONTACT/Contact"))
+const Experience = React.lazy(() => import("./PAGE/EXPERIENCE/Experience"))
+const Navbar = React.lazy(() => import("./components/NAVBAR/Navbar"))
 // console.log("Home",Home)
 export const DownWardWith2000 = keyframes`
 
@@ -44,19 +47,36 @@ function App(): JSX.Element {
     <Box className="App" border={"1px"} overflowX={"hidden"} borderColor={"#edf9fe"} bg={"thistheme.background"} >
       <HashRouter basename="/">
         <Reveal duration={2000} keyframes={DownWardWith2000}>
-          <Navbar />
+          <Suspense>
+
+            <Navbar />
+          </Suspense>
         </Reveal>
 
         <Routes>
           <Route path='/' element={
             // <Splash />
-            <Home />
+            <Suspense>
+              <Home />
+            </Suspense>
           } />
-          <Route path='/home' element={<Home />} />
-          <Route path='/education' element={<Education />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path='/experience' element={<Experience />} />
+          <Route path='/home' element={<Suspense><Home /></Suspense>} />
+          <Route path='/education' element={<Suspense><Education /></Suspense>} />
+          <Route path='/projects' element={
+            <Suspense>
+              <Projects />
+            </Suspense>
+          } />
+          <Route path='/Contact' element={
+            <Suspense>
+              <Contact />
+            </Suspense>
+          } />
+          <Route path='/experience' element={
+            <Suspense>
+              <Experience />
+            </Suspense>
+          } />
         </Routes>
         <Footer theme={appTheme} />
         <TopButton theme={appTheme} />
--- src/components/CompetitiveSites/CompetitiveSites.tsx
+++ src/components/CompetitiveSites/CompetitiveSites.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Box,
   Link,
   Tooltip,
   Img,
